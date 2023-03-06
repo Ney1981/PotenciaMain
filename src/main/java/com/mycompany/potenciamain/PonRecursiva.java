ae358446b811a38eaee2044708366397603f693c
Ney1981 <joanmartwhit500@gmail.com>
2023-03-06 01:28:46 -0500

    Segundo taller del primer corte

---
+++ src/main/java/com/mycompany/potenciamain/PonRecursiva.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.potenciamain;
+
+public class PonRecursiva {
+     public int potencia(int a, int b){
+        if(a==1){
+            return 1;   
+        } 
+        if(b==1){
+            return a;
+        }return a * potencia(a,b-1);
+    }
+}
---
+++ src/main/java/com/mycompany/potenciamain/PotenciaMain.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.potenciamain;
+
+public class PotenciaMain {
+
+    public static void main(String[] args) {
+        PonRecursiva pot = new PonRecursiva();
+     System.out.println(pot.potencia(5,2));
+    }
+}
